4648d4f175eb22dbf4b540e826e9f8ef7b323b10
Niraporn Kovitaya <bam_28@hotmail.com>
2022-03-25 12:31:23 +0700

    Add api to post updated board from frontend to backend

---
+++ src/main/java/io/muzoo/ssc/project/backend/game/BoardPost.java
@@ -0,0 +1,25 @@
+package io.muzoo.ssc.project.backend.game;
+
+import io.muzoo.ssc.project.backend.SimpleResponseDTO;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+public class BoardPost {
+
+    @PostMapping("/api/board")
+    public SimpleResponseDTO updateBoard(@RequestBody List<List<String>> payload) {
+        System.out.println(payload);
+        CalculateBoard calculator = new CalculateBoard.CalculateBoardBuilder().board(payload).build();
+        Map<Integer, List<List<Integer>>> possibilities = calculator.AllPossibilities();
+        return SimpleResponseDTO
+                .builder()
+                .success(true)
+                .message("Board is updated to backend to computation for next round")
+                .build();
+    }
+}
