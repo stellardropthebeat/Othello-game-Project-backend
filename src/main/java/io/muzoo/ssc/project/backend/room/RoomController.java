3564ff443ae943034d93ef1225cfd7d7effef91e
DROPTHEBEAT44 <nutchapol1111@gmail.com>
2022-04-01 23:38:54 +0700

    Change a little bit of thing

--- src/main/java/io/muzoo/ssc/project/backend/room/RoomController.java
+++ src/main/java/io/muzoo/ssc/project/backend/room/RoomController.java
@@ -4,11 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+@RestController
 public class RoomController {
 
     @Autowired
@@ -17,7 +19,7 @@ public class RoomController {
     @PostMapping("/api/join-room")
     public RoomResponseDTO getFirstAvailableRoom(@RequestBody Map<String,Object> payload) {
         for (Room room : roomRepository.findAll()) {
-            if (!room.getPlayer1().isEmpty() && room.getPlayer2().isEmpty()){
+            if ((room.getPlayer1()!=null) && (room.getPlayer2()==null)){
                 room.setPlayer2((String) payload.get("username"));
                 return RoomResponseDTO.builder().availableRoom(room.getId()).build();
             }
