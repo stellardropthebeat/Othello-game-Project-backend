dc38bb213407a49396fa231c17dc1d039348e4f4
DROPTHEBEAT44 <nutchapol1111@gmail.com>
2022-04-01 22:53:59 +0700

    Change GENERATEEDVALUE to identity

--- src/main/java/io/muzoo/ssc/project/backend/room/Room.java
+++ src/main/java/io/muzoo/ssc/project/backend/room/Room.java
@@ -12,7 +12,7 @@ import javax.persistence.*;
 public class Room {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
     private long id;
 
     private String player1;
