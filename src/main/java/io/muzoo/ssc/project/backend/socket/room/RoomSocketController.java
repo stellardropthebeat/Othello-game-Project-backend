511297aafdaff38445bd6bde8b1ce23537f11bb5
DROPTHEBEAT44 <nutchapol1111@gmail.com>
2022-04-02 19:53:01 +0700

    Fix condition of host leaving the room and room is not closed properly

--- src/main/java/io/muzoo/ssc/project/backend/socket/room/RoomSocketController.java
+++ src/main/java/io/muzoo/ssc/project/backend/socket/room/RoomSocketController.java
@@ -2,6 +2,7 @@ package io.muzoo.ssc.project.backend.socket.room;
 
 import io.muzoo.ssc.project.backend.room.Room;
 import io.muzoo.ssc.project.backend.room.RoomRepository;
+import io.muzoo.ssc.project.backend.room.RoomResponseDTO;
 import net.minidev.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
@@ -9,7 +10,9 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 
+import java.util.*;
 import java.util.Map;
+import java.util.NoSuchElementException;
 
 @Controller
 public class RoomSocketController {
@@ -19,12 +22,16 @@ public class RoomSocketController {
 
     @MessageMapping("/room/{RoomId}")
     @SendTo("/topic/wait/{RoomId}")
-    public JSONObject wait(@DestinationVariable("RoomId") String RoomId, Map<String,Object> payload) throws Exception{
+    public JSONObject wait(@DestinationVariable("RoomId") String RoomId, Map<String,Object> payload) throws NoSuchElementException {
 
-        long roomId = Long.parseLong(String.valueOf(payload.get("roomId")));
-        Room room = roomRepository.findById(roomId).get();
-        String player1 = room.getPlayer1();
-        String player2 = room.getPlayer2();
+        Long roomId = Long.parseLong(String.valueOf(payload.get("roomId")));
+        String player1 = null;
+        String player2 = null;
+        if(roomRepository.findById(roomId).isPresent()){
+            Room room = roomRepository.findById(roomId).get();
+            player1 = room.getPlayer1();
+            player2 = room.getPlayer2();
+        }
 
         JSONObject object = new JSONObject();
         object.put("player1", player1);
